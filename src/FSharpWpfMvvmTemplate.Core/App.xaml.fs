08d3e505f38021a9ea6c26db10e964b2e19f920f
Dan Mohl <dmohl@yahoo.com>
2010-05-30 09:15:55 -0500

    Added initial code

---
+++ src/FSharpWpfMvvmTemplate.Core/App.xaml.fs
@@ -0,0 +1,50 @@
+﻿namespace FSharpWpfMvvmTemplate
+
+open System
+open System.Globalization
+open System.Windows
+open System.Windows.Markup
+//open FSharpWpfMvvmTemplate.ViewModel
+
+type App = 
+    static member Initialize () = 
+        FrameworkElement.LanguageProperty.OverrideMetadata(
+            typeof<FrameworkElement>,
+            new FrameworkPropertyMetadata(
+                XmlLanguage.GetLanguage(
+                    CultureInfo.CurrentCulture.IetfLanguageTag)))
+    new () = {}  then App.Initialize()
+    inherit Application
+
+//
+//        protected override void OnStartup(StartupEventArgs e)
+//        {
+//            base.OnStartup(e);
+//
+//            MainWindow window = new MainWindow();
+//
+//            // Create the ViewModel to which 
+//            // the main window binds.
+//            string path = "Data/customers.xml";
+//            var viewModel = new MainWindowViewModel(path);
+//
+//            // When the ViewModel asks to be closed, 
+//            // close the window.
+//            EventHandler handler = null;
+//            handler = delegate
+//            {
+//                viewModel.RequestClose -= handler;
+//                window.Close();
+//            };
+//            viewModel.RequestClose += handler;
+//
+//            // Allow all controls in the window to 
+//            // bind to the ViewModel by setting the 
+//            // DataContext, which propagates down 
+//            // the element tree.
+//            window.DataContext = viewModel;
+//
+//            window.Show();
+//        }
+//    }
+//}
