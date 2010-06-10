96e0e2dc43c2080b3e3f0ca93ca612da874cbaa9
Dan Mohl <dmohl@yahoo.com>
2010-06-10 07:27:14 -0500

    Continued porting the MVVM example to F#

--- src/FSharpWpfMvvmTemplate.ViewModel/MainWindowCommandViewModel.fs
+++ src/FSharpWpfMvvmTemplate.ViewModel/MainWindowCommandViewModel.fs
@@ -11,103 +11,62 @@ open FSharpWpfMvvmTemplate.DataAccess
 open FSharpWpfMvvmTemplate.Model
 open FSharpWpfMvvmTemplate.Properties
 
-//namespace DemoApp.ViewModel
-//{
-//    public class MainWindowCommandViewModel
-//    {
-//        ObservableCollection<WorkspaceViewModel> _workspaces;
-//        readonly CustomerRepository _customerRepository;
-//
-//        public MainWindowCommandViewModel(CustomerRepository customerRepository)
-//        {
-//            _customerRepository = customerRepository;
-//        }
-//
-//        public List<CommandViewModel> CreateCommands()
-//        {
-//            return new List<CommandViewModel>
-//            {
-//                new CommandViewModel(
-//                    Strings.MainWindowViewModel_Command_ViewAllCustomers,
-//                    new RelayCommand(param => this.ShowAllCustomers())),
-//
-//                new CommandViewModel(
-//                    Strings.MainWindowViewModel_Command_CreateNewCustomer,
-//                    new RelayCommand(param => this.CreateNewCustomer()))
-//            };
-//        }
-//
-//        #region Private Helpers
-//
-//        void CreateNewCustomer()
-//        {
-//            Customer newCustomer = Customer.CreateNewCustomer();
-//            CustomerViewModel workspace = new CustomerViewModel(newCustomer, _customerRepository);
-//            this.Workspaces.Add(workspace);
-//            this.SetActiveWorkspace(workspace);
-//        }
-//
-//        void ShowAllCustomers()
-//        {
-//            AllCustomersViewModel workspace =
-//                this.Workspaces.FirstOrDefault(vm => vm is AllCustomersViewModel)
-//                as AllCustomersViewModel;
-//
-//            if (workspace == null)
-//            {
-//                workspace = new AllCustomersViewModel(_customerRepository);
-//                this.Workspaces.Add(workspace);
-//            }
-//
-//            this.SetActiveWorkspace(workspace);
-//        }
-//
-//        void SetActiveWorkspace(WorkspaceViewModel workspace)
-//        {
-//            Debug.Assert(this.Workspaces.Contains(workspace));
-//
-//            ICollectionView collectionView = CollectionViewSource.GetDefaultView(this.Workspaces);
-//            if (collectionView != null)
-//                collectionView.MoveCurrentTo(workspace);
-//        }
-//
-//        #endregion // Private Helpers
-//
-//        void OnWorkspacesChanged(object sender, NotifyCollectionChangedEventArgs e)
-//        {
-//            if (e.NewItems != null && e.NewItems.Count != 0)
-//                foreach (WorkspaceViewModel workspace in e.NewItems)
-//                    workspace.RequestClose += this.OnWorkspaceRequestClose;
-//
-//            if (e.OldItems != null && e.OldItems.Count != 0)
-//                foreach (WorkspaceViewModel workspace in e.OldItems)
-//                    workspace.RequestClose -= this.OnWorkspaceRequestClose;
-//        }
-//
-//        void OnWorkspaceRequestClose(object sender, EventArgs e)
-//        {
-//            WorkspaceViewModel workspace = sender as WorkspaceViewModel;
-//            workspace.Dispose();
-//            this.Workspaces.Remove(workspace);
-//        }
-//
-//        /// <summary>
-//        /// Returns the collection of available workspaces to display.
-//        /// A 'workspace' is a ViewModel that can request to be closed.
-//        /// </summary>
-//        public ObservableCollection<WorkspaceViewModel> Workspaces
-//        {
-//            get
-//            {
-//                if (_workspaces == null)
-//                {
-//                    _workspaces = new ObservableCollection<WorkspaceViewModel>();
-//                    _workspaces.CollectionChanged += this.OnWorkspacesChanged;
-//                }
-//                return _workspaces;
-//            }
-//        }
-//
-//    }
-//}
-
+type MainWindowCommandViewModel(_customerRepository) =
+    [<DefaultValue(false)>] 
+    val mutable _workspaces : ObservableCollection<WorkspaceViewModel>
+    member x.CreateCommands () = 
+        let commandViewModels = new List<CommandViewModel>()
+        commandViewModels.Add(
+            new CommandViewModel(
+                Strings.MainWindowViewModel_Command_ViewAllCustomers,
+                new RelayCommand(fun param -> 
+                    MainWindowCommandViewModelMod.ShowAllCustomers())))
+        commandViewModels.Add(
+            new CommandViewModel(
+                Strings.MainWindowViewModel_Command_CreateNewCustomer,
+                new RelayCommand(fun param -> 
+                    MainWindowCommandViewModelMod.CreateNewCustomer())))
+    member x.Workspaces 
+        with get() = match x._workspaces with
+                     | null -> 
+                        x._workspaces <- new ObservableCollection<WorkspaceViewModel>()
+                        x._workspaces.CollectionChanged.AddHandler x.OnWorkspacesChanged
+                        x._workspaces
+                     | _ -> x._workspaces
+    member x.CreateNewCustomer () = 
+        let newCustomer = Customer.CreateNewCustomer()
+        let workspace = 
+            new CustomerViewModel(newCustomer, _customerRepository)
+            |> x.Workspaces.Add
+        x.SetActiveWorkspace workspace    
+    member x.ShowAllCustomers () =
+        let workspace = x.Workspaces 
+                        |> Seq.tryFind (fun vm -> 
+                            vm.GetType() = AllCustomersViewModel) 
+                            :?> AllCustomersViewModel
+        match workspace with
+        | null -> let newWorkspace = 
+                      new AllCustomersViewModel(_customerRepository)
+                      |> x.Workspaces.Add
+                  newWorkspace
+        | _ -> workspace
+        |> x.SetActiveWorkspace
+    member x.SetActiveWorkspace workspace =
+        let collectionView = 
+            CollectionViewSource.GetDefaultView(x.Workspaces)
+        match collectionView with 
+        | null -> true
+        | _ -> collectionView.MoveCurrentTo workspace
+        |> ignore
+    member x.OnWorkspacesChanged (sender:obj) (e:NotifyCollectionChangedEventArgs) =
+        match e with
+        | _ when e.NewItems <> null && e.NewItems.Count <> 0 ->
+            for workspace in e.NewItems do 
+                workspace.RequestClose.AddHandler x.OnWorkspaceRequestClose
+        | _ when e.OldItems <> null && e.OldItems.Count <> 0 ->
+            for workspace in e.OldItems do 
+                workspace.RequestClose.RemoveHandler x.OnWorkspaceRequestClose
+    member x.OnWorkspaceRequestClose (sender:obj) (e : EventArgs) =
+        let workspace = sender :?> WorkspaceViewModel
+        do workspace.Dispose()
+        x.Workspaces.Remove workspace
