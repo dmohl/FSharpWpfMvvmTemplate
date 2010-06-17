caaa64535ae70d0f9d327cd2becbe11d22ea297b
Dan Mohl <dmohl@yahoo.com>
2010-06-17 13:46:49 -0500

    Reverted to a much more simple example

---
+++ src/FSharpWpfMvvmTemplate.App/App.fs
@@ -0,0 +1,17 @@
+﻿module FSharpWpfMvvmTemplate
+
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+open FSharpWpfMvvmTemplate.ViewModel
+
+// Create the View and bind it to the View Model
+let mainView = 
+    Application.LoadComponent(new System.Uri("/FSharpWpfMvvmTemplate.View;component/MainView.xaml", System.UriKind.Relative)) :?> Window
+mainView.DataContext <- new MainViewModel() :> obj
+
+// Application Entry point
+[<STAThread>]
+[<EntryPoint>]
+let main(_) = (new Application()).Run(mainView)
