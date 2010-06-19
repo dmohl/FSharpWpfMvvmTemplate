b23e9e9be7a2d2ba0c5455ec8c934c388ec4e371
Dan Mohl <dmohl@yahoo.com>
2010-06-19 14:32:12 -0500

    Added functionality to set the frame source via the view model

--- src/FSharpWpfMvvmTemplate.ViewModel/ExpenseItHomeViewModel.fs
+++ src/FSharpWpfMvvmTemplate.ViewModel/ExpenseItHomeViewModel.fs
@@ -1,12 +1,40 @@
 ﻿namespace FSharpWpfMvvmTemplate.ViewModel
 
 open System
+open System.Xml
 open System.Windows
+open System.Windows.Data
 open System.Windows.Input
 open System.ComponentModel
 
 type ExpenseItHomeViewModel() =   
-    inherit ViewModelBase()       
+    inherit ViewModelBase() 
+    member x.ExpenseData = 
+        let doc = new XmlDocument()
+        doc.LoadXml(
+            "<Expenses xmlns=\"\">
+                <Person Name=\"Mike\" Department=\"Legal\">
+                    <Expense ExpenseType=\"Lunch\" ExpenseAmount=\"50\" />
+                    <Expense ExpenseType=\"Transportation\" ExpenseAmount=\"50\" />
+                </Person>
+                <Person Name=\"Lisa\" Department=\"Marketing\">
+                    <Expense ExpenseType=\"Document printing\"
+                        ExpenseAmount=\"50\"/>
+                    <Expense ExpenseType=\"Gift\" ExpenseAmount=\"125\" />
+                </Person>
+                <Person Name=\"John\" Department=\"Engineering\">
+                    <Expense ExpenseType=\"Magazine subscription\" 
+                        ExpenseAmount=\"50\"/>
+                    <Expense ExpenseType=\"New machine\" ExpenseAmount=\"600\" />
+                    <Expense ExpenseType=\"Software\" ExpenseAmount=\"500\" />
+                </Person>
+                <Person Name=\"Mary\" Department=\"Finance\">
+                    <Expense ExpenseType=\"Dinner\" ExpenseAmount=\"100\" />
+                </Person>
+            </Expenses>")
+        let xmlProvider = new XmlDataProvider()
+        xmlProvider.Document <- doc
+        xmlProvider
     member x.ViewCommand = 
         new RelayCommand ((fun canExecute -> true), (fun action -> x.ShowMessage)) 
 
--- src/FSharpWpfMvvmTemplate.ViewModel/MainWindowViewModel.fs
+++ src/FSharpWpfMvvmTemplate.ViewModel/MainWindowViewModel.fs
@@ -3,16 +3,24 @@
 open System
 open System.Collections.ObjectModel
 open System.Windows
+open System.Windows.Data
 open System.Windows.Input
 open System.ComponentModel
 
-type MainWindowViewModel() =
-//    let collectionView : ICollectionView =  
-//        let views = new ObservableCollection<ViewModelBase>()
-//        views.Add(new ExpenseItHomeViewModel())              
-//        views.Add(new ExpenseReportViewModel())  
-//                    
-//    member ObservableCollection<ViewModelBase> Views = x.views
-//    member x.SelectedView = collectionView.CurrentItem :> ViewModelBase
-    member public x.ShowMessage = do MessageBox.Show("test") |> ignore
+type MainWindowViewModel =
+    [<DefaultValue(false)>] val mutable _views : ObservableCollection<string>   
+    new() as x = {_views = new ObservableCollection<string>()} then x.Initialize()
+    member x.Initialize() =
+        let defaultView = "ExpenseItHome.xaml"
+        x._views.Add(defaultView)              
+        x._views.Add("ExpenseReport.xaml") 
+        x.SetActiveView defaultView
+    member x.SelectedView = 
+        let collectionView = CollectionViewSource.GetDefaultView x.Views
+        collectionView.CurrentItem :?> string
+    member x.Views = x._views
+    member x.SetActiveView view =
+        let collectionView = CollectionViewSource.GetDefaultView x.Views
+        if collectionView <> null then collectionView.MoveCurrentTo view |> ignore
+
 
