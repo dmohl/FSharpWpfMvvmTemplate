e67aa2d3547bed257ee5bd96aacefa3a5ff7b09a
Dan Mohl <dmohl@yahoo.com>
2011-07-03 13:22:08 -0500

    Added an example of a value converter

--- src/App/ViewModel/ExpenseItHomeViewModel.fs
+++ src/App/ViewModel/ExpenseItHomeViewModel.fs
@@ -10,22 +10,38 @@ open System.Collections.ObjectModel
 open FSharpWpfMvvmTemplate.Model
 open FSharpWpfMvvmTemplate.Repository
 
+type ApprovalStatus =
+    | Approved
+    | Rejected
+
 type ExpenseItHomeViewModel(expenseReportRepository : ExpenseReportRepository) =   
     inherit ViewModelBase()
+    let mutable lastApprovalDisplayMessage = ""
     let mutable selectedExpenseReport = 
         {Name=""; Department=""; ExpenseLineItems = []}
     new () = ExpenseItHomeViewModel(ExpenseReportRepository())
     member x.ExpenseReports = 
         new ObservableCollection<ExpenseReport>(
             expenseReportRepository.GetAll())
+    member x.HandleApprovalAction approvalStatus name =
+        match approvalStatus with
+        | ApprovalStatus.Approved -> 
+            x.LastApprovalDisplayMessage <- sprintf "Expense report approved for %s" name
+        | ApprovalStatus.Rejected ->
+            x.LastApprovalDisplayMessage <- sprintf "Expense report rejected for %s" name
     member x.ApproveExpenseReportCommand = 
-        new RelayCommand ((fun canExecute -> true), (fun action -> x.ApproveExpenseReport)) 
+        new RelayCommand ((fun canExecute -> true), 
+            (fun action -> x.HandleApprovalAction ApprovalStatus.Approved x.SelectedExpenseReport.Name)) 
+    member x.RejectExpenseReportCommand = 
+        new RelayCommand ((fun canExecute -> true), 
+            (fun action -> x.HandleApprovalAction ApprovalStatus.Rejected x.SelectedExpenseReport.Name)) 
     member x.SelectedExpenseReport 
         with get () = selectedExpenseReport
         and set value = 
             selectedExpenseReport <- value
             x.OnPropertyChanged "SelectedExpenseReport"
-    member x.ApproveExpenseReport = 
-        // Note: This is for example purposes only.  It is not a good practice to 
-        //       use MessageBox.Show in the ViewModel. 
-        MessageBox.Show(sprintf "Expense report approved for %s" x.SelectedExpenseReport.Name) |> ignore
+    member x.LastApprovalDisplayMessage 
+        with get() = lastApprovalDisplayMessage
+        and set value = 
+            lastApprovalDisplayMessage <- value 
+            x.OnPropertyChanged "LastApprovalDisplayMessage"
